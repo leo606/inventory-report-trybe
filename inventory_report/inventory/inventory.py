e7b5b8f5259d4c4a5feda8ddef9c561c93cefc24
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-24 16:53:56 -0300

    feat: inventory json reader

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -19,14 +20,39 @@ class CSV_Reader:
             return list(file_data)
 
 
+class JSON_Reader:
+    def read(path):
+        with open(path) as file:
+            file_data = json.load(file)
+            return file_data
+
+
+class XML_Reader:
+    pass
+
+
+class Serialize:
+    def serialize(data_listed, type):
+        if type == "simples":
+            return SimpleReport.generate(data_listed)
+        elif type == "completo":
+            return CompleteReport.generate(data_listed)
+
+
 class Inventory:
     @classmethod
     def import_data(cls, path, type):
-        file_extension = path.split(".")[-1]
+        file_extension = path.split(".")[-1].lower()
 
         if file_extension == "csv":
             data_listed = FileReader(path, CSV_Reader).read()
-            if type == "simples":
-                return SimpleReport.generate(data_listed)
-            elif type == "completo":
-                return CompleteReport.generate(data_listed)
+            return Serialize.serialize(data_listed, type)
+        elif file_extension == "json":
+            data_listed = FileReader(path, JSON_Reader).read()
+            return Serialize.serialize(data_listed, type)
+        elif file_extension == "xml":
+            data_listed = FileReader(path, XML_Reader).read()
+            return Serialize.serialize(data_listed, type)
+
+
+print(Inventory.import_data("inventory_report/data/inventory.csv", "completo"))

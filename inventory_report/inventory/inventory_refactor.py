56327ccc6b883783cec4f99ba96cbd2acde9b7a0
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-03-01 08:43:23 -0300

    feat: inventory refactor (iterable)

---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,29 @@
+from collections.abc import Iterable
+from inventory_report.inventory.inventory_iterator import InventoryIterator
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Serialize:
+    serialize_types = {"simples": SimpleReport, "completo": CompleteReport}
+
+    @classmethod
+    def serialize(cls, data_listed, type):
+        return cls.serialize_types[type].generate(data_listed)
+
+
+class InventoryRefactor(Iterable):
+    def __init__(self, importer):
+        self.importer = importer
+        self.data = []
+
+    def import_data(self, path, type):
+        data_listed = self.importer.import_data(path)
+        self.data.extend(data_listed)
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
+
+
+# iterator = iter(inventory)
+# first_item = next(iterator)
